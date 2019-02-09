eb7754258028f99867c057c082cdc7eea731199c
Michael Warren <michael@queenand.co>
2019-02-09 16:12:33 +0900

    Create generator.js

---
+++ js/generator.js
@@ -0,0 +1,48 @@
+$(document).ready(function() {
+
+    function getQuote() {
+        var quotes = [{
+            quote: "こんにづは！"
+        }, {
+            quote: "どんだんだば！"
+        }, {
+            quote: "わいは！"
+        }, {
+            quote: "まんずな！"
+        }, {
+            quote: "あずましいじゃ！"
+        }, {
+            quote: "まいね、まいね！"
+        }, {
+            quote: "ぉんろ〜！"
+        }, {
+            quote: "どうしちゃば！"
+        }, {
+            quote: "なんぼめごこだば！ "
+        }, {
+            quote: "なのどんず、<br>まのどんず　"
+        }, {
+            quote: "腹チェじゃ！"
+        }, ];
+
+        var sourceLength = quotes.length;
+        var randomNumber = Math.floor(Math.random() * sourceLength);
+
+        for (var i = 0; i <= sourceLength; i++) {
+            var newQuoteText = quotes[randomNumber].quote;
+            var newQuoteName = quotes[randomNumber].name;
+        }
+
+        var timeAnimation = 200;
+        var quoteContainer = $("#quoteContainer");
+        quoteContainer.fadeOut(timeAnimation, function() {
+            quoteContainer.html('');
+            quoteContainer.append('<p>' + newQuoteText + '</p>');
+            quoteContainer.fadeIn(timeAnimation);
+        }); //end of fadeOut
+    } //end of getQuote
+    getQuote();
+
+    $("#quoteButton").click(getQuote);
+
+}); //end of document.ready
