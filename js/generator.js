0027087ffe785da15adc5b4d1b709c76c0b59dc9
Michael Warren <michael@queenand.co>
2019-02-12 15:01:16 +0900

    Update generator.js

--- js/generator.js
+++ js/generator.js
@@ -2,59 +2,76 @@ $(document).ready(function() {
 
     function getQuote() {
         var quotes = [{
-            quote: "こんにづは！"
+            quote: "こんにづは！",
+            name: "こんにちは！"
         }, {
-            quote: "どんだんだば！"
+            quote: "どんだんだば！",
+            name: "なんてことだ！"
         }, {
-            quote: "わいは！"
+            quote: "わいは！",
+            name: "(驚いた時の感嘆詞)"
         }, {
-            quote: "まんずな！"
+            quote: "まんずな！",
+            name: "そうだよね！"
         }, {
-            quote: "あずましいじゃ！"
+            quote: "あずましいじゃ！",
+            name: "気持ちいいね！"
         }, {
-            quote: "まいね、まいね！"
+            quote: "まいね、まいね！",
+            name: "ダメ、ダメ！"
         }, {
-            quote: "ぉんろ〜！"
+            quote: "ぉんろ〜！",
+            name: "あら〜！"
         }, {
-            quote: "どうしちゃば！"
+            quote: "どうしちゃば！",
+            name: ""
         }, {
-            quote: "なんぼめごこだば！ "
+            quote: "なんぼめごこだば！ ",
+            name: "なんて可愛い子だね！"
         }, {
-            quote: "まのどんず　"
+            quote: "じょんばけつ",
+            name: "出っ張ったおしり"
         }, {
-            quote: "腹チェじゃ！"
+            quote: "腹チェじゃ！",
+            name: "お腹いっぱい！"
         }, {
-            quote: "もつけだはんで！"
+            quote: "もつけだはんで！",
+            name: ""
         }, {
-            quote: "あっちゃ！"
+            quote: "あっちゃ！",
+            name: "お母さん！"
         }, {
-            quote: "いーいーいー！"
+            quote: "いーいーいー！",
+            name: "あーあーあー！"
         }, {
-            quote: "いがめんち！"
+            quote: "いがめんち！",
+            name: "イカの足(ゲソ)を細かく刻んで油で揚げたり焼いたりした郷土料理"
         }, {
-            quote: "いだわしい！"
+            quote: "いだわしい！",
+            name: "もったいない！"
         }, {
-            quote: "けっ！"
+            quote: "けっ！",
+            name: "食べなさい！"
         }, ];
 
-        var sourceLength = quotes.length;
-        var randomNumber = Math.floor(Math.random() * sourceLength);
+    var sourceLength = quotes.length;
+    var randomNumber = Math.floor(Math.random() * sourceLength);
 
-        for (var i = 0; i <= sourceLength; i++) {
-            var newQuoteText = quotes[randomNumber].quote;
-            var newQuoteName = quotes[randomNumber].name;
-        }
-
-        var timeAnimation = 200;
-        var quoteContainer = $("#quoteContainer");
-        quoteContainer.fadeOut(timeAnimation, function() {
-            quoteContainer.html('');
-            quoteContainer.append('<p>' + newQuoteText + '</p>');
-            quoteContainer.fadeIn(timeAnimation);
-        }); //end of fadeOut
+    for (var i=0; i<=sourceLength; i++) {
+      var newQuoteText = quotes[randomNumber].quote;
+      var newQuoteName = quotes[randomNumber].name;
+    }
+    
+    var timeAnimation = 200;
+    var quoteContainer = $("#quoteContainer");
+      quoteContainer.fadeOut(timeAnimation, function() {
+        quoteContainer.html('');
+        quoteContainer.append('<p>' + newQuoteText + '</p>' + '<p id="quoteName">' + newQuoteName + '</p>');
+        quoteContainer.fadeIn(timeAnimation);
+      }); //end of fadeOut
     } //end of getQuote
-    getQuote();
-
-    $("#quoteButton").click(getQuote);
+getQuote();
+  
+$("#quoteButton").click(getQuote);
 
 }); //end of document.ready
